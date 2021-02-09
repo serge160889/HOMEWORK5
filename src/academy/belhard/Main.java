166e58131a3320137be01909c844b23a27ff7a6e
serge160889 <serge_voit_2006@mail.ru>
2021-02-09 22:08:52 +0300

    finish

--- src/academy/belhard/Main.java
+++ src/academy/belhard/Main.java
@@ -8,10 +8,15 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Person p1=new Person("Sergei","Voitehovich");
-        User u1=new User("Sergei","Voitehovich","serge_voit_2006@mail.ru");
+        User u1=new User("Sergei","Voitehovich","serge_voit_2006@mail.ru","family");
         String p2= p1.getFullName();
         System.out.println(p2);
         String u2= u1.getFullInfo();
-        System.out.printf(u2);
+        System.out.println(u2);
+        boolean e2=u1.isPasswordCorrect("family");
+        System.out.println(e2);
+        boolean e3= u1.isPasswordCorrect("star");
+        System.out.println(e3);
+
     }
 }
--- src/entity/User.java
+++ src/entity/User.java
@@ -1,23 +1,40 @@
 package entity;
 
-public class User extends Person{
-     private String email;
-     private String password;
+public class User extends Person {
+    private String email;
+    private String password;
 
-    public User(String firstName, String lastName, String email) {
+    public User(String firstName, String lastName, String email, String password) {
         super(firstName, lastName);
         this.email = email;
-    }
-    public String getEmail(){return email;}
-    public void setEmail(String email){this.email=email;}
-    public String getPassword(){return password;}
-    public void setPassword(String password){this.password=password;}
-    public String getFullInfo(){
-        return
-                "Имя:  "+firstName+" " +lastName+"\n"+ "E-mail: "+email;
-    }
-    public boolean isPasswordCorrect(String family){
-         return
+        this.password = password;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullInfo() {
+        return
+                "Имя:  " + firstName + " " + lastName + "\n" + "E-mail: " + email;
+    }
+
+
+    public boolean isPasswordCorrect(String z) {
+        return this.password.equals(z);
+
+    }
 }
+
