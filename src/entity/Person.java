c0fe61e6dfb445f56b8d88126fc78426ae4f53c3
serge160889 <serge_voit_2006@mail.ru>
2021-02-09 22:13:09 +0300

    finish

--- src/entity/Person.java
+++ src/entity/Person.java
@@ -8,10 +8,7 @@ package entity;
         this.firstName=firstName;
         this.lastName=lastName;
     }
-//    public String getFirstName(){return firstName;}
-//    public void setFirstName(String firstName){this.firstName=firstName;}
-//    public String getLastName(){return lastName;}
-//    public void setLastName(String lastName){this.lastName=lastName;}
+
          public String getFullName(){
             return firstName+" "+lastName;
          }
